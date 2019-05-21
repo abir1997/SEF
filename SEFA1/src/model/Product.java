890e0929bce8875d8c25cda1225662f8ff738f5e
s3707180 <s7@rmit.edu.au>
2019-05-21 23:14:49 +1000

    added equals and hashcode functions to correct map and equals functions

--- SEFA1/src/model/Product.java
+++ SEFA1/src/model/Product.java
@@ -52,4 +52,39 @@ public class Product {
 	{
 		this.price = price;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((productID == null) ? 0 : productID.hashCode());
+		result = prime * result + quantity;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		if (productID == null) {
+			if (other.productID != null)
+				return false;
+		} else if (!productID.equals(other.productID))
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		return true;
+	}
+	
+	
 }
