61bb1ccb848a37348090ddaffcd5d4d412964468
s3707180 <s7@rmit.edu.au>
2019-05-26 08:45:22 +1000

    Minor, added getCustomers() function

--- SEFA1/src/dataAccess/UserDataAccess.java
+++ SEFA1/src/dataAccess/UserDataAccess.java
@@ -2,11 +2,23 @@ package dataAccess;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
+import model.Customer;
 import model.User;
 
 public class UserDataAccess {
 
 	public static Map<Integer, User> users = new HashMap<>();
 
+	public Set<Customer> getAllCustomers(){
+		Set<Customer> customers = new TreeSet<>();
+		for (User user: users.values()) {
+			if (user instanceof Customer) {
+				customers.add((Customer)user);
+			}
+		}
+		return customers;
+	}
 }
